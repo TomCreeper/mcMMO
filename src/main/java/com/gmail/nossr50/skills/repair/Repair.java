ea02404d81286d3c9ea4ed906a73ea6ac8f6c538
nossr50 <nossr50@gmail.com>
2019-03-05 21:29:41 -0800

    Fix Repair Mastery zero initialize

--- src/main/java/com/gmail/nossr50/skills/repair/Repair.java
+++ src/main/java/com/gmail/nossr50/skills/repair/Repair.java
@@ -5,8 +5,8 @@ import com.gmail.nossr50.config.MainConfig;
 import org.bukkit.Material;
 
 public class Repair {
-    public static int    repairMasteryMaxBonusLevel = AdvancedConfig.getInstance().getRepairMasteryMaxLevel();
-    public static double repairMasteryMaxBonus      = AdvancedConfig.getInstance().getRepairMasteryMaxBonus();
+    //public static int    repairMasteryMaxBonusLevel = AdvancedConfig.getInstance().getRepairMasteryMaxLevel();
+    //public static double repairMasteryMaxBonus      = AdvancedConfig.getInstance().getRepairMasteryMaxBonus();
 
     public static Material anvilMaterial  = MainConfig.getInstance().getRepairAnvilMaterial();
 }
