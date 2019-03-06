18f27b60c7e8c07c5c851f3cfa96fa43a5b1aa6a
nossr50 <nossr50@gmail.com>
2019-03-05 21:38:50 -0800

    Fixed zero initialize for Smelting's Fuel Efficiency

--- src/main/java/com/gmail/nossr50/skills/smelting/SmeltingManager.java
+++ src/main/java/com/gmail/nossr50/skills/smelting/SmeltingManager.java
@@ -1,5 +1,6 @@
 package com.gmail.nossr50.skills.smelting;
 
+import com.gmail.nossr50.config.AdvancedConfig;
 import com.gmail.nossr50.config.MainConfig;
 import com.gmail.nossr50.datatypes.experience.XPGainReason;
 import com.gmail.nossr50.datatypes.experience.XPGainSource;
